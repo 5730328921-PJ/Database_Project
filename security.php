d56008b33b30deef00f48109f978210da665fbea
PH7 <prin.jit@gmail.com>
2017-04-28 15:40:29 +0700

    change login method in security.php

--- security.php
+++ security.php
@@ -5,13 +5,13 @@
     DEFINE('DB_DATABASE', 'CPstudent CARE');
     $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-    $q = "SELECT login, password FROM teacher";
+    $q = sprintf("SELECT password FROM teacher WHERE login = %s", $_GET["login"]);
     $result = $mysqli->query($q);
     $count = 1;
     $total = mysqli_num_rows($result);
     while ($row = $result->fetch_assoc()) {
-        if ($row["login"] == $_GET["login"] && $row["password"] == $_GET["password"]) {
-            printf("<meta http-equiv=\"refresh\" content=\"0;url=index.php?login=%s\" />", $row["login"]);
+        if ($row["password"] == $_GET["password"]) {
+            printf("<meta http-equiv=\"refresh\" content=\"0;url=index.php?login=%s\" />", $_GET["login"]);
         }
     }
     echo "Invalid username and password";
